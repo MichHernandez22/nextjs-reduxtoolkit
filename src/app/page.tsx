660b7db6f56b8b6728f01701dd25fcb130e18a54
Ana Michelle Lopez Hernandez <ing.amichellelopezhdz@gmail.com>
2025-08-14 13:19:10 -0600

    consume api users and set styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,45 @@
 "use client";
 import { useAppDispatch, useAppSelector } from "@/redux/hooks"
 import { increment, decrement } from "@/redux/features/counterSlice";
+import { useGetUsersQuery } from "@/redux/services/userApi";
 
 function HomePage() {
 
   const count = useAppSelector(state => state.counterReducer.counter)
+  const {data, error, isLoading, isFetching} = useGetUsersQuery(null)
+
+  if(isLoading || isFetching) return <p>Loading...</p>
+  if(error) return <p>Error</p>
+
   const dispatch = useAppDispatch()
 
   return (
-    <div>
-      <h1>total: {count}</h1>
-      <button onClick={() =>{
-        dispatch(increment())
-      }}>Increment</button>
-      <br/>
-      <button onClick={() => {
-        dispatch(decrement())
-      }}>Decrement</button>
+    <div className="">
+      <h1 className="text-center text-2xl">total: {count}</h1>
+      <div className="flex justify-center gap-x-2 w-full ">
+        <button className="bg-green-500 px-3 py-2 rounded-md" onClick={() =>{
+          dispatch(increment())
+        }}>Increment</button>
+        <br/>
+        <button className="bg-blue-500 px-3 py-2 rounded-md" onClick={() => {
+          dispatch(decrement())
+        }}>Decrement</button>
+      </div>
+      
+
+      <div className="grid grid-cols-3 mx-auto gap-3">
+        {
+          data?.map(user => (
+            <div className="bg-zinc-800 p-4">
+              <p>{user.name}</p>
+              <p>{user.username}</p>
+              <p>{user.email}</p>
+            </div>
+          ))
+        }
+      </div>
+
+      
 
     </div>   
   )
